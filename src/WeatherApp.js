5710ef94796891564a7d620cc75dcc267b5a0955
maJestic17 <77455932+maJestic17@users.noreply.github.com>
2021-08-30 01:40:11 +0300

    Moved my React App to VS Code

---
+++ src/WeatherApp.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  let weatherData = {
+    city: "Los Angeles",
+    temperature: 30,
+    date: "Thursday 11:00",
+    description: "Sunny",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 80,
+    wind: 10
+  };
+
+  return (
+    <div className="Weather">
+      <form className="mb-3">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city.."
+              className="form-control"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="form-control btn btn-warning shadow w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <div className="overview">
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+          <li>Humidity: {weatherData.humidity}%</li>
+          <li>Wind: {weatherData.wind} km/h</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix weather-temperature">
+            <img
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <strong>{weatherData.temperature}</strong>
+              <span className="units">
+                <a href="/">°C</a> | <a href="/">°F</a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="Footer">
+        <p className="footer">
+          <a href="https://github.com/maJestic17/my-weather-project">
+            Open-source code
+          </a>{" "}
+          by Daria Sukhacheva
+        </p>
+      </div>
+    </div>
+  );
+}
